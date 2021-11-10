82cdf7badb00dd4c5b1025afaead88309bf8410c
AndrewGronin <andrygronin3@gmail.com>
2021-11-10 18:31:41 +0300

    *

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {gql, useQuery} from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {users{
+    firstName
+}}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {loading, error, data} = useQuery(EXCHANGE_RATES);
+    console.log(data)
+    return (
+        <>
+            <>dadsa</>
+            <form
+            >
+                <button onClick={()=>
+                console.log("sfasfasf")} type="button"> cockc </button>
+            </form>
+        </>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = new ApolloClient({
+    uri: 'https://localhost:5001/graphql/',
+    cache: new InMemoryCache(),
+});
+
+render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
